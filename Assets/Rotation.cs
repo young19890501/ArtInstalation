606000ce6b5467953754434a3b49f69f8a4ce491
Yang Liu <Yang_Liu@thecdm.ca>
2016-07-24 19:26:17 -0700

    Y

---
+++ Assets/Rotation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.Rotate(Vector3.up,0.5f);
+	}
+}
